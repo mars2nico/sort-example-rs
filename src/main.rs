5e30c66f8c0703e6641a38560b931398df72033e
Daichi Misaka <misaka6085@gmail.com>
2023-08-13 14:27:57 +0900

    N: 10 --> 100000

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,27 @@ mod sort;
 
 fn main() {
     let mut rng = thread_rng();
-    let choices: Vec<_> = (1..11).collect();
+    let choices: Vec<_> = (1..10001).collect();
     let mut base_arr = Vec::new();
-    for _ in 0..10 {
+    for _ in 0..10000 {
         base_arr.push(*choices.choose(&mut rng).unwrap());
     }
-    let mut arr1 = base_arr.clone();
-    let mut arr2 = base_arr.clone();
     {
-        println!(" arr(before) = {:?}", &arr1[0..10]);
+        let mut arr = base_arr.clone();
+        println!(" arr(before) = {:?}", &arr[0..10]);
 
         let start = Instant::now();
-        sort::insertion_sort(&mut arr1);
+        sort::insertion_sort(&mut arr);
 
-        println!(" arr(after ) = {:?}, elapsed = {:?}", &arr1[0..10], start.elapsed());
+        println!(" arr(after ) = {:?}, elapsed = {:?}", &arr[0..10], start.elapsed());
     }
     {
-        println!(" arr(before) = {:?}", &arr2[0..10]);
+        let mut arr = base_arr.clone();
+        println!(" arr(before) = {:?}", &arr[0..10]);
 
         let start = Instant::now();
-        sort::shell_sort(&mut arr2);
+        sort::shell_sort(&mut arr);
 
-        println!(" arr(after ) = {:?}, elapsed = {:?}", &arr2[0..10], start.elapsed());
-    }
-    for i in 0..base_arr.len() {
-        if arr1[i] != arr2[i] {
-            println!("!ERR at {:?}", i);
-        }
+        println!(" arr(after ) = {:?}, elapsed = {:?}", &arr[0..10], start.elapsed());
     }
 }
